b595b46f208d15f9140626eb6b8f01e6f191a927
Aditya <adityawarekar1984@gmail.com>
2025-07-01 17:57:16 +0530

    Some issues ared done

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-const  express =  require("express");
+const express = require("express");
+
+const urlRoute = require("./routes/url");
+const { connectToMongoDB } = require("./connect");
+const URL = require('./models/url');
 
 const app = express();
 const PORT = 8001;
 
-app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`))
+
+connectToMongoDB('mongodb://127.0.0.1:27017/short-url')
+  .then(() => console.log("Mongodb connected"))
+  .catch((err) => console.error("MongoDB connection failed:", err));
+
+app.use(express.json());
+
+
+app.use("/url", urlRoute);
+
+app.get('/:shortId', (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await.URL.findOneAndUpdate(
+       {
+          shortId,
+       },
+
+     { $push: {
+        visitHistory:  {
+            timestamp: Date.now(),
+        }
+    },
+ }
+);
+res.redirect(entry.redirectURL);
+});
+
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
