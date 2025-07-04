a128e036f621cfe2d8214ad7ad682541f4c05393
Aditya <adityawarekar1984@gmail.com>
2025-07-04 22:22:04 +0530

    ejs rendering

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const urlRoute = require("./routes/url");
 const { connectToMongoDB } = require("./connect");
 const URL = require("./models/url");
+const staticRoute = require('./routes/staticRouter')
 
 const app = express();
 const PORT = 8001;
@@ -14,17 +15,14 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
 
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/test", async (req, res) => {
-   const allUrls = await  URL.find({});
-   return res.render("home", {
-      urls: allUrls,
-   });
-});
+
 
 app.set("view engine", "ejs");
 app.set('views', path.resolve("./views"));
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
 
 app.get("/:shortId", async (req, res) => {
